f79cb9a762b7c53e65125cfd0563b5625351e3de
for-you-Nikita <troickiy09@gmail.com>
2025-06-15 13:54:52 +0300

    commit bot

---
+++ handlers/user_handlers.py
@@ -0,0 +1,60 @@
+from aiogram import F, Router, Bot
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+from keyboards.keyboards import yes_no_kb, game_kb
+from lexicon.lexicon import LEXICON_RU
+from services.services import get_winner, get_bot_choice
+
+router = Router()
+
+# Этот хэндлер срабатывает на команду /start
+@router.message(CommandStart())
+async def pracess_start_command(message: Message):
+    await message.answer(
+        text=LEXICON_RU['/start'],
+        reply_markup=yes_no_kb
+    )
+
+# Этот хэндлер срабатывает на команду /help
+@router.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(
+        text=LEXICON_RU['/help'],
+        reply_markup=yes_no_kb
+    )
+# Этот хэндлер будет срабатывать на команду "/delmenu"
+# и удалять кнопку Menu c командами
+@router.message(Command(commands='delmenu'))
+async def del_main_menu(message: Message, bot: Bot):
+    await bot.delete_my_commands()
+    await message.answer(text='Кнопка "Menu" удалена')
+
+
+# Этот хэндлер срабатывает на согласие пользователя играть в игру
+@router.message(F.text == LEXICON_RU['yes_button'])
+async def process_yes_answer(message: Message):
+    await message.answer(
+        text=LEXICON_RU['yes'],
+        reply_markup=game_kb
+    )
+
+# Этот хэндлер срабатывает на отказ пользователя играть в игру
+@router.message(F.text == LEXICON_RU['no_button'])
+async def process_no_answer(message: Message):
+    await message.answer(
+        text=LEXICON_RU['no']
+    )
+
+# Этот хэндлер срабатывает на любую из игровых кнопок
+@router.message(F.text.in_([LEXICON_RU['rock'],
+                            LEXICON_RU['paper'],
+                            LEXICON_RU['scissors']]))
+async def process_game_button(message: Message):
+    bot_choice = get_bot_choice()
+    await message.answer(
+        text=f'{LEXICON_RU["bot_choice"]} '
+             f'- {LEXICON_RU[bot_choice]}'
+    )
+    winner = get_winner(message.text, bot_choice)
+    await message.answer(text=LEXICON_RU[winner],
+                         reply_markup=yes_no_kb)
